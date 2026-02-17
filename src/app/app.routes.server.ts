9b5c7aff021e9469f75035854eca1782e9f28398
bhavanimeka-codeline <bhavani.meka@codeline.in>
2026-02-17 19:41:33 +0530

    changes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'blog/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: ':slug',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
