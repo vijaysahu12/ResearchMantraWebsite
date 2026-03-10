681e4d2ccd4b63fd4a01a5892b2ad832887ea19b
Ajith-codeline <ajith.codeline@gmail.com>
2026-03-10 14:53:59 +0530

    Header Dropdown issue fixed.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy, HostListener, ElementRef, inject } from '@angular/core';
 
 import { RouterLink } from '@angular/router';
 
@@ -12,13 +12,36 @@ import { RouterLink } from '@angular/router';
 })
 export class HeaderComponent {
     isMenuOpen = signal(false);
+    isBlogsOpen = signal(false);
     isComplianceOpen = signal(false);
+private elementRef = inject(ElementRef);
+    @HostListener('document:click', ['$event'])
+    onDocumentClick(event: PointerEvent) {
+        // If the click is NOT inside the header component, close everything
+        const clickedInside = this.elementRef.nativeElement.contains(event.target);
+
+        if (!clickedInside) {
+            this.closeAllMenus();
+        }
+    }
+
+    private closeAllMenus() {
+        this.isMenuOpen.set(false);
+        this.isBlogsOpen.set(false);
+        this.isComplianceOpen.set(false);
+    }
 
     toggleMenu() {
         this.isMenuOpen.update(open => !open);
         if (!this.isMenuOpen()) {
             this.isComplianceOpen.set(false);
         }
+    }
+
+    toggleBlogs(event: Event) {
+        event.stopPropagation();
+        this.isBlogsOpen.update(open => !open);
+        this.isComplianceOpen.set(false); // Close the other one if this opens
     }
 
     toggleCompliance(event: Event) {
