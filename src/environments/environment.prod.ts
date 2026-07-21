245c6178c686b90f0360e757984898912ac994a8
vjmac <vijay.sahu12@gmail.com>
2026-07-21 06:19:00 +0530

    blogs api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   signalRURL: 'https://crmapi.researchmantra.in/notification',
   apiurl: 'https://crmapi.researchmantra.in/api/',
+  websiteBlogApiUrl: 'https://crmapi.researchmantra.in/api/',
   mobilePushNotificationUrl: 'https://product.researchmantra.in/api/',
   mobileBaseUrl: 'https://product.researchmantra.in/api/',
   gatewayUrl: 'https://gateway.researchmantra.in/gateway/',
